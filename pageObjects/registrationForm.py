5e377bac7853f27272cf8bd109444c143d87b01d
sudaryadi <sudaryadi47@gmail.com>
2023-06-27 13:37:40 +0700

    First commit "Add project structure"

--- pageObjects/registrationForm.py
+++ pageObjects/registrationForm.py
@@ -2,12 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class Registration:
-    menu_forms_class = "card-up"
-    submenu_practiceForm_class = "btn btn-light "
+    menu_forms_xpath = "/html/body/div[2]/div/div/div[2]/div/div[2]"
+    submenu_practiceForm_xpath = "/html/body/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div"
     txt_firstName_id = "firstName"
     txt_lastName_id = "lastName"
     txt_email_id = "userEmail"
-    radio_male_id = "gender-radio-1"
+    radio_male_xpath = "/html/body/div[2]/div/div/div[2]/div[2]/div[2]/form/div[3]/div[2]/div[1]"
     txt_mobile_id = "userNumber"
     txt_DoB_id = "dateOfBirth"
     txt_subjects_id = "subjectsContainer"
@@ -20,14 +20,14 @@ class Registration:
     item_jaipur_xpath = "/html/body/div[2]/div/div/div[2]/div[2]/div[2]/form/div[10]/div[3]/div/div/div[1]/div[1]"
     btn_submit_id = "submit"
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self, setup):
+        self.driver = setup
 
     def select_forms(self):
-        self.driver.find_element(By.ID, self.menu_forms_class).click()
+        self.driver.find_element(By.XPATH, self.menu_forms_xpath).click()
 
     def select_practice_form(self):
-        self.driver.find_element(By.ID, self.submenu_practiceForm_class).click()
+        self.driver.find_element(By.XPATH, self.submenu_practiceForm_xpath).click()
 
     def input_first_name(self, first):
         self.driver.find_element(By.ID, self.txt_firstName_id).send_keys(first)
@@ -39,10 +39,10 @@ class Registration:
         self.driver.find_element(By.ID, self.txt_email_id).send_keys(email)
 
     def select_gender_male(self):
-        self.driver.find_element(By.ID, self.radio_male_id).click()
+        self.driver.find_element(By.XPATH, self.radio_male_xpath).click()
 
     def input_phone_number(self, mobile):
-        self.driver.find_element(By.ID, self.input_phone_number).send_keys(mobile)
+        self.driver.find_element(By.ID, self.txt_mobile_id).send_keys(mobile)
 
     def input_dob(self, birthdate):
         self.driver.find_element(By.ID, self.txt_DoB_id).send_keys(birthdate)
@@ -65,7 +65,7 @@ class Registration:
 
     def select_city(self):
         self.driver.find_element(By.ID, self.list_city_id).click()
-        self.driver.find_element(By.ID, self.item_jaipur_xpath).click()
+        self.driver.find_element(By.XPATH, self.item_jaipur_xpath).click()
 
     def click_submit(self):
         self.driver.find_element(By.ID, self.btn_submit_id).click()
--- testCase/studentRegister.py
+++ testCase/studentRegister.py
@@ -1,2 +1,51 @@
+from pageObjects.registrationForm import Registration
+from time import sleep
 
-from selenium import webdriver
+
+class TestRegister:
+    base_url = "https://demoqa.com/"
+    first = "Uchiha"
+    last = "Senju"
+    email = "senju@gmail.com"
+    male = "male"
+    mobile = "08992995299"
+    birthdate = "15 November 1993"
+    subjects = "Ilmu Komputer"
+    picture = "images/img.png"
+    address = "Bandung"
+
+    def test_filling_registration_form(self, setup):
+        self.driver = setup
+        self.driver.get(self.base_url)
+        self.driver.maximize_window()
+        # import method dari folder page objects #
+        self.reg = Registration(self.driver)
+        sleep(3)
+        self.reg.select_forms()
+        sleep(3)
+        self.reg.select_practice_form()
+        sleep(3)
+        self.reg.input_first_name(self.first)
+        self.reg.input_last_name(self.last)
+        sleep(3)
+        self.reg.input_email(self.email)
+        sleep(3)
+        self.reg.select_gender_male()
+        sleep(3)
+        self.reg.input_phone_number(self.mobile)
+        sleep(3)
+        self.reg.input_dob(self.birthdate)
+        sleep(3)
+        self.reg.input_subjects(self.subjects)
+        sleep(3)
+        self.reg.select_hobbies()
+        sleep(3)
+        self.reg.input_picture(self.picture)
+        sleep(3)
+        self.reg.input_address(self.address)
+        sleep(3)
+        self.reg.select_stat()
+        sleep(3)
+        self.reg.select_city()
+        sleep(3)
+        self.reg.click_submit()
